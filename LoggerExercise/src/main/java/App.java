c602ec48ff6e0f271637cbdcdab35a5db3fb56ef
HeldanaG <heldanagmariam@gmail.com>
2025-06-09 15:01:36 -0500

    Completed MOD1 - Exercise 1 - Logger Exercise

--- LoggerExercise/src/main/java/App.java
+++ LoggerExercise/src/main/java/App.java
@@ -1,6 +1,8 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 public class App {
+
     final static Logger logger = LogManager.getLogger(App.class);
     public static void main(String[] args) {
         logMeLikeYouDo("☕ ");
